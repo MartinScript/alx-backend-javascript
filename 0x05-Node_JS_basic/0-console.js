5cd5748e1ecac34f48d64d83990ade0d5e223567
MartinScript <martinsojoola2@gmail.com>
2024-09-30 09:26:58 +0100

    solve tasks

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author Martins Ojo <https://github.com/MartinScript>
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
